e08d0f17281076185481fc653d791c9db3f119f7
hmader <halina.mader@gmail.com>
2016-07-28 16:46:01 -0700

    adding possible_discrepancy

---
+++ possible_discrepancy/js-2/global-vars.js
@@ -0,0 +1,52 @@
+var maxWidth = 800;
+var width;
+var zipcodeWidth;
+
+function getWidth() {
+    if ($(window).width() >= maxWidth) {
+        width = maxWidth;
+        zipcodeWidth = 500;
+    } else {
+        width = $(window).width();
+        zipcodeWidth = $(window).width();
+    }
+}
+
+// global dataset variables
+
+// These are the datasets after filtering/ formatting
+var mapByCounty;
+var nestByCounty;
+var nestByYear;
+
+// this is the raw json data, before filtering/ formatting
+var allCountiesDataset;
+var zipcodesDataset;
+var thisCountyDataset;
+var countyMap;
+
+var races = ["asian", "black", "hispanic", "white", "other"]; // these are the races that are represented with cancer data
+var genders = ["female", "male"];
+var population = ["asian", "black", "hispanic", "native_american", "white"]; // these are the races that make up the population in the population data
+
+var raceColors1 = ["#f8f7ce", "#ffe59a", "#ffca7d", "#f6755f", "#ffaf71"];
+var raceColors2 = ["#f8f7ce", "#ffe59a", "#ffca7d", "#ffaf71", "#f6755f"];
+
+
+// tooltip var
+var tooltip = d3.select("body")
+    .append("div")
+    .attr("class", "tooltip");
+
+// years range
+var years = ["2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013"];
+
+// to uppercase all words in a string - removes "-" and "_"
+var uppercase = function (wordString) {
+    var string = wordString.replace(/-/g, " ").replace(/_/g, " ").split(" ");
+    var finalString = "";
+    string.forEach(function (d) {
+        finalString = finalString.concat(d[0].toUpperCase() + d.substring(1, d.length) + " ");
+    });
+    return (finalString);
+};
---
+++ possible_discrepancy/js-2/json-api.js
@@ -0,0 +1,82 @@
+var countyNumber = 360;
+var startYear = 2004;
+var endYear = 2013;
+var cancerType = 50;
+
+
+/*=====================================================================
+ loaded()
+======================================================================*/
+function loaded(error, allCounties, allZips, thisCounty, countyNameMap) { // load in all of the data to be used in the viz
+
+    // set all of the data to global data variables
+    allCountiesDataset = allCounties;
+    zipcodesDataset = allZips;
+    thisCountyDataset = thisCounty;
+    countyMap = countyNameMap;
+
+    $(".county-name-text").text(countyMap[countyNumber] + " County");
+
+    $(".cancer-type-text").text(thisCountyDataset.cancer.name + " Cancer");
+    // formatting
+    setupData(allCountiesDataset);
+    // call the function(s) - unless we have multiple pages or triggers, just call all of the chart functions
+    getWidth();
+    callAllCharts();
+}
+/*=====================================================================
+ queue Data
+=====================================================================*/
+queue()
+    .defer(d3.json, 'http://scanportal.org/json/county/raw/' + cancerType) // all counties in FL
+    .defer(d3.json, 'http://scanportal.org/json/zipcode/raw/' + countyNumber + '/' + cancerType) // all zipcodes
+    .defer(d3.json, 'http://scanportal.org/json/county/' + countyNumber + '/' + cancerType + '/' + startYear + '/' + endYear) // this county
+    .defer(d3.json, 'http://scanportal.org/api/counties') // county id - name map
+    .await(loaded);
+
+
+function setupData(d) {
+
+    mapByCounty = d3.map(d, function (d) {
+        return d.county
+    });
+
+    nestByCounty = d3.nest().key(function (d) {
+        //                console.log(d.county);
+        return d.county;
+    }).entries(d);
+
+    nestByYear = d3.nest().key(function (d) {
+        //                console.log(d.county);
+        return d.year;
+    }).entries(d);
+
+    console.log("ALL", allCountiesDataset);
+    console.log("NBY", nestByYear);
+    console.log("NBC", nestByCounty);
+    console.log("MBC", mapByCounty);
+    console.log("THIS C", thisCountyDataset);
+    console.log("ZIPS", zipcodesDataset);
+}
+
+/*=====================================================================
+ drawing charts
+=====================================================================*/
+
+function callAllCharts() {
+    callCountArea("#countAreaChart");
+}
+
+/*=====================================================================
+ window resize
+=====================================================================*/
+
+$(window).resize(function () {
+    clearAllsvg();
+    getWidth();
+    callAllCharts();
+});
+
+function clearAllsvg() {
+    d3.selectAll('svg').remove();
+};
